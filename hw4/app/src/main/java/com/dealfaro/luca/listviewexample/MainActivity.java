f130598d029204a4bf73308fe1b74a8381460265
Ryan Teves <rteves@ucsc.edu>
2017-05-04 18:53:19 -0700

    got change activity to work

--- hw4/app/src/main/java/com/dealfaro/luca/listviewexample/MainActivity.java
+++ hw4/app/src/main/java/com/dealfaro/luca/listviewexample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dealfaro.luca.listviewexample;
 
 import android.content.Context;
+import android.content.Intent;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -116,6 +117,10 @@ public class MainActivity extends AppCompatActivity {
                     int duration = Toast.LENGTH_SHORT;
                     Toast toast = Toast.makeText(context, s, duration);
                     toast.show();
+
+                    Intent intent = new Intent(context, ReaderActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
                 }
             });
 
