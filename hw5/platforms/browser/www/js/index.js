e2b99ab4bd8c39866a3897c09c78d67581212df4
Ryan Teves <rteves@ucsc.edu>
2017-05-14 18:16:21 -0700

    got the tiles to move

--- hw5/platforms/browser/www/js/index.js
+++ hw5/platforms/browser/www/js/index.js
@@ -60,12 +60,42 @@ var app = function() {
 
     self.shuffle = function(i, j) {
         // You need to implement this.
+       if (self.vue.cellColor(i, j+1) == 'black'){
+            self.vue.swap(4*i+j, 4*i+(j+1));
+        } else if (self.cellColor(i, j-1) == 'black'){
+            self.vue.swap(4*i+j, 4*i+(j-1));
+        } else if (self.cellColor(i+1, j) == 'black'){
+            self.vue.swap(4*i+j, 4*(i+1)+j)
+        } else if (self.cellColor(i-1, j) == 'black'){
+            self.vue.swap(4*i+j, 4*(i-1)+j);
+        } else {
+            console.log("You can't move!");
+        }
 	        console.log("Shuffle:" + i + ", " + j);
     };
-
+	
+     // Swap the cells
+   
     self.scramble = function() {
         // Read the Wikipedia article.  If you just randomize,
         // the resulting puzzle may not be solvable.
+    };
+
+      // Swap the cells
+    self.swap = function(current, target) {
+        temp = self.vue.board[current];
+        Vue.set(self.vue.board, current, self.vue.board[target]);
+        Vue.set(self.vue.board, target, temp);
+    }
+
+    // Returns the (String) color associated with that cell
+    self.cellColor = function(i, j) {
+        try {
+            return (Object.values(self.vue.board[4*i+j]));
+        } catch (exception) {
+            //console.error('NULL ERROR', exception.message);
+        }
+
     };
 
     self.vue = new Vue({
@@ -95,7 +125,9 @@ var app = function() {
         methods: {
             reset: self.reset,
             shuffle: self.shuffle,
-            scramble: self.scramble
+            scramble: self.scramble,
+	    swap: self.swap,
+	    cellColor: self.cellColor,
         }
 
     });
