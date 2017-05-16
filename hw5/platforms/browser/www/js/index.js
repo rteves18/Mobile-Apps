2f7fcc3a7b29b7182ec8894a0b24910edfa739b2
Ryan Teves <rteves@ucsc.edu>
2017-05-16 09:35:02 -0700

    almost done

--- hw5/platforms/browser/www/js/index.js
+++ hw5/platforms/browser/www/js/index.js
@@ -60,38 +60,121 @@ var app = function() {
 
     self.shuffle = function(i, j) {
         // You need to implement this.
-       if (self.vue.cellColor(i, j+1) == 'black'){
-            self.vue.swap(4*i+j, 4*i+(j+1));
-        } else if (self.cellColor(i, j-1) == 'black'){
-            self.vue.swap(4*i+j, 4*i+(j-1));
-        } else if (self.cellColor(i+1, j) == 'black'){
-            self.vue.swap(4*i+j, 4*(i+1)+j)
-        } else if (self.cellColor(i-1, j) == 'black'){
-            self.vue.swap(4*i+j, 4*(i-1)+j);
+       if (self.vue.getKey(i, j+1) == '0'){
+            self.vue.switchCell(4*i+j, 4*i+(j+1));
+        } else if (self.getKey(i, j-1) == '0'){
+            self.vue.switchCell(4*i+j, 4*i+(j-1));
+        } else if (self.getKey(i+1, j) == '0'){
+            self.vue.switchCell(4*i+j, 4*(i+1)+j)
+        } else if (self.getKey(i-1, j) == '0'){
+            self.vue.switchCell(4*i+j, 4*(i-1)+j);
         } else {
-            console.log("You can't move!");
+            console.log("Can't move from there");
         }
-	        console.log("Shuffle:" + i + ", " + j);
     };
-	
-     // Swap the cells
-   
-    self.scramble = function() {
-        // Read the Wikipedia article.  If you just randomize,
-        // the resulting puzzle may not be solvable.
-    };
+    self.scrambleExe = function() {
+        self.vue.ScrambledBoard = [];
+        // Shuffle by Fisher-Yates
+        for (count = self.vue.board.length - 1; count >= 0; count--) {
+            var j = Math.floor(Math.random() * (count + 1));
+            self.vue.switchCell(count, j);
 
-      // Swap the cells
-    self.swap = function(current, target) {
+            // Add scramble array
+            self.vue.ScrambledBoard.push(Object.keys(self.vue.board[count])[0]);
+        }
+        self.vue.ScrambledBoard.reverse();
+        console.log("Inversion Count: " + 
+        self.getInversion(self.vue.ScrambledBoard));
+        console.log("Empty cell is " + self.locateBlackTile(self.vue.ScrambledBoard));
+    }
+	
+    // Prints elements in array for testing purposes
+    self.printArray = function(array) {
+        for (i = 0; i < array.length; i++) {
+            console.log(array[i]);
+        }
+    }
+
+
+ self.scramble = function() {
+	//execute scramble
+        self.scrambleExe();
+	//continue to scramble if not scramblable
+        while ( self.isSolvable(self.vue.ScrambledBoard) == false ) {
+            self.scrambleExe();
+        }
+    };
+    // Determines if scrambled board is solvable
+    self.isSolvable = function(boardArray) {
+        // Get inversion count
+        var isInvCntEven = self.isEven(self.getInversion(boardArray));
+
+        // Get the position of the blank cell
+        var isEmptyCellEvenRow = self.locateBlackTile(boardArray);
+
+        // Board is solvable if:
+        // - blank cell is on an even row counting from bottom && num inversion is odd
+        // OR - blank cell is on an odd row counting from bottom && num inversion is even
+        if ( (isInvCntEven && !isEmptyCellEvenRow) || 
+             (!isInvCntEven && isEmptyCellEvenRow) ) {
+            return true;
+            console.log("scramble is solvable");
+        } else {
+            return false;
+            console.log("scramble is unsolvable");
+        }
+    }
+
+    // Count inversions in board array
+    self.getInversion = function(boardArray) {
+        inc = 0;
+        for (var i = 0; i < boardArray.length - 1; i++) {
+            for (var j = i + 1; j < boardArray.length; j++) {
+                if (boardArray[j] && boardArray[i] && boardArray[i] > boardArray[j])
+                    inc++;
+            }
+        }
+        return inc;
+    }
+
+    //checks if even or not
+    self.isEven = function (number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Finds position of blank cell
+    self.locateBlackTile = function(boardArray) {
+        var index = 0;
+
+        //Located black 0 tile 
+        for (i = 0; i < boardArray.length; i++) {
+            if (boardArray[i] == 0) {
+                index = i;
+            }
+        }
+        if ( (index > -1 && index < 4) || 
+             (index > 7 && index < 12) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //switch cell for tiles to move 
+    self.switchCell = function(current, target) {
         temp = self.vue.board[current];
         Vue.set(self.vue.board, current, self.vue.board[target]);
         Vue.set(self.vue.board, target, temp);
     }
 
-    // Returns the (String) color associated with that cell
-    self.cellColor = function(i, j) {
+    // Returns cell key 
+    self.getKey = function(i, j) {
         try {
-            return (Object.values(self.vue.board[4*i+j]));
+            return (Object.keys(self.vue.board[4*i+j]));
         } catch (exception) {
             //console.error('NULL ERROR', exception.message);
         }
@@ -126,8 +209,9 @@ var app = function() {
             reset: self.reset,
             shuffle: self.shuffle,
             scramble: self.scramble,
-	    swap: self.swap,
-	    cellColor: self.cellColor,
+	    switchCell: self.switchCell,
+	    getKey: self.getKey,
+	    printArray: self.printArray,
         }
 
     });
